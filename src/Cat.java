e52b399207bdc2e0faf513fd58ac5816b09931d5
haytastan <haytastan@gmail.com>
2020-11-14 00:33:09 +0300

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,7 @@
+// Subclass (inherited from Animal)
+class Cat extends Animal {
+    public void animalSound() {
+        // The body of animalSound() is provided here
+        System.out.println("The cat says: miyav");
+    }
+}
---
+++ src/MyMainClass.java
@@ -0,0 +1,7 @@
+class MyMainClass {
+    public static void main(String[] args) {
+        Cat myCat = new Cat(); // Create a Cat object
+        myCat.animalSound();
+        myCat.sleep();
+    }
+}
